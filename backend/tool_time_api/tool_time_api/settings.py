4bef3178370d88d9940153a7f28da97ec3e16a97
qbic22562 <mateusz.kubica99@gmail.com>
2020-01-11 20:00:57 +0100

    Added cors-headers again

--- backend/tool_time_api/tool_time_api/settings.py
+++ backend/tool_time_api/tool_time_api/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     'accounts',
     'equipment',
     'django_apscheduler',
+    'corsheaders',
 ]
 
 SCHEDULER_CONFIG = {
@@ -67,7 +68,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
 ]
+
+CORS_ORIGIN_ALLOW_ALL = True
 
 ROOT_URLCONF = 'tool_time_api.urls'
 
